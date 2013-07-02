dcb90a2b268e8cad4a4e2e3651443688bd602ddc
Robert Hartley <hartleyr@hotmail.com>
2013-07-01 22:14:13 -0400

    Merge

--- Neopic/Types/ModelData.cs
+++ Neopic/Types/ModelData.cs
@@ -8,7 +8,12 @@ namespace Neopic.Types
 {
     public class ModelData
     {
+<<<<<<< HEAD:Neopic/Types/ModelData.cs
         public Dimensions Dimensions { get; set; }
         public SparseBitArray Data { get; set; }
+=======
+        public Dimensions Dimensions { get; }
+        public SparseBitArray Data { get; }
+>>>>>>> fc79ff46cfd3e619ef16a7b85bee705bfb9a870c:Neopic/Types/SensoryData.cs
     }
 }
